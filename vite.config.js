b08ebac59ede73c09f324e912dba2f055e90b40c
antonmak1 <anthonymaxgit@gmail.com>
2025-11-02 17:58:43 +0500

    update game

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: 'src',
+  root: "src",
   build: {
-    outDir: '../dist',
+    outDir: "../dist",
     emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === "apples.html") {
+            return "apples.html";
+          }
+          return "assets/[name]-[hash][extname]";
+        },
+      },
+    },
   },
+  publicDir: false,
   css: {
     preprocessorOptions: {
-      scss: {}
-    }
-  }
-})
+      scss: {},
+    },
+  },
+});
