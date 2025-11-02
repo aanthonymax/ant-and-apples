3cb359921197fac20342360fd2d77c88e866a341
antonmak1 <anthonymaxgit@gmail.com>
2025-11-02 18:11:06 +0500

    update apples.html

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,11 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "src",
+  publicDir: "../public",
   build: {
     outDir: "../dist",
     emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          if (assetInfo.name === "apples.html") {
-            return "apples.html";
-          }
-          return "assets/[name]-[hash][extname]";
-        },
-      },
-    },
   },
-  publicDir: false,
   css: {
     preprocessorOptions: {
       scss: {},
